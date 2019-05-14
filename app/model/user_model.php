369859942583e85b910dbcccce2a118f2dab4c53
pablonura91 <pablonura91@gmail.com>
2019-05-14 07:43:09 +0200

    refactor code

--- app/model/user_model.php
+++ app/model/user_model.php
@@ -3,87 +3,97 @@ namespace App\Model;
 
 use App\Lib\Database;
 use App\Lib\Response;
+use DateTime;
 
 class UserModel
 {
     private $db;
     private $table = 'Notes';
     private $response;
-    
+
     public function __CONSTRUCT()
     {
-        return $this->db = Database::StartUp();
+        $this->db = Database::StartUp();
         $this->response = new Response();
     }
 
     public function GetAll()
     {
-		try
-		{
-			$result = array();
+        try
+        {
+            $result = array();
 
-			$stm = $this->db->prepare("SELECT * FROM $this->table");
-			$stm->execute();
-            
-			$this->response->setResponse(true);
+            $stm = $this->db->prepare("SELECT * FROM $this->table");
+            $stm->execute();
+
+            $this->response->setResponse(true);
             $this->response->result = $stm->fetchAll();
-            
+
             return $this->response;
-		}
-		catch(Exception $e)
-		{
-			$this->response->setResponse(false, $e->getMessage());
+        }
+        catch(Exception $e)
+        {
+            $this->response->setResponse(false, $e->getMessage());
             return $this->response;
-		}
+        }
     }
-    
+
     public function Get($id)
     {
-		try
-		{
-			$result = array();
+        try
+        {
+            $result = array();
 
-			$stm = $this->db->prepare("SELECT * FROM $this->table WHERE id = ?");
-			$stm->execute(array($id));
+            $stm = $this->db->prepare("SELECT * FROM $this->table WHERE id = ?");
+            $stm->execute(array($id));
 
-			$this->response->setResponse(true);
+            $this->response->setResponse(true);
             $this->response->result = $stm->fetch();
-            
+
             return $this->response;
-		}
-		catch(Exception $e)
-		{
-			$this->response->setResponse(false, $e->getMessage());
+        }
+        catch(Exception $e)
+        {
+            $this->response->setResponse(false, $e->getMessage());
             return $this->response;
-		}  
+        }
     }
-    
+
     public function InsertOrUpdate($data)
     {
-		try 
-		{
+        try
+        {
+            $createData = date("Y-m-d H:i:s");
+
             if(isset($data['id']))
             {
                 $sql = "UPDATE $this->table SET 
-                            Nombre          = ?, 
-                            Apellido        = ?,
-                            Correo          = ?,
-                            Sexo            = ?,
-                            Sueldo          = ?,
-                            Profesion_id    = ?,
-                            FechaNacimiento = ?
+                            title          = ?, 
+                            content        = ?,
+                            private        = ?,
+                            tag1           = ?,
+                            tag2           = ?,
+                            tag3           = ?,
+                            tag4           = ?,
+                            book           = ?,
+                            creationDate            = ?,
+                            lastModificationDate    = ?,
+                            user           = ?
                         WHERE id = ?";
-                
+
                 $this->db->prepare($sql)
-                     ->execute(
+                    ->execute(
                         array(
-                            $data['Nombre'], 
-                            $data['Apellido'],
-                            $data['Correo'],
-                            $data['Sexo'],
-                            $data['Sueldo'],
-                            $data['Profesion_id'],
-                            $data['FechaNacimiento'],
+                            $data['title'],
+                            $data['content'],
+                            $data['private'],
+                            $data['tag1'],
+                            $data['tag2'],
+                            $data['tag3'],
+                            $data['tag4'],
+                            $data['book'],
+                            $createData,
+                            $data['user'],
                             $data['id']
                         )
                     );
@@ -91,46 +101,89 @@ class UserModel
             else
             {
                 $sql = "INSERT INTO $this->table
-                            (Nombre, Apellido, Correo, Sexo, Sueldo, Profesion_id, FechaNacimiento, FechaRegistro)
-                            VALUES (?,?,?,?,?,?,?,?)";
-                
+                            (title, content, private, tag1, tag2, tag3, tag4, book, creationDate, user)
+                            VALUES (?,?,?,?,?,?,?,?,?,?)";
+
                 $this->db->prepare($sql)
-                     ->execute(
+                    ->execute(
                         array(
-                            $data['Nombre'], 
-                            $data['Apellido'],
-                            $data['Correo'],
-                            $data['Sexo'],
-                            $data['Sueldo'],
-                            $data['Profesion_id'],
-                            $data['FechaNacimiento'],
-                            date('Y-m-d')
+                            $data['title'],
+                            $data['content'],
+                            $data['private'],
+                            $data['tag1'],
+                            $data['tag2'],
+                            $data['tag3'],
+                            $data['tag4'],
+                            $data['book'],
+                            $createData,
+                            $data['user']
                         )
-                    ); 
+                    );
             }
-            
-			$this->response->setResponse(true);
+
+            $this->response->setResponse(true);
             return $this->response;
-		}catch (Exception $e) 
-		{
+        }catch (Exception $e)
+        {
             $this->response->setResponse(false, $e->getMessage());
-		}
+        }
     }
-    
+
     public function Delete($id)
     {
-		try 
-		{
-			$stm = $this->db
-			            ->prepare("DELETE FROM $this->table WHERE id = ?");			          
+        try
+        {
+            $stm = $this->db
+                ->prepare("DELETE FROM $this->table WHERE id = ?");
 
-			$stm->execute(array($id));
-            
-			$this->response->setResponse(true);
+            $stm->execute(array($id));
+
+            $this->response->setResponse(true);
             return $this->response;
-		} catch (Exception $e) 
-		{
-			$this->response->setResponse(false, $e->getMessage());
-		}
+        } catch (Exception $e)
+        {
+            $this->response->setResponse(false, $e->getMessage());
+        }
+    }
+
+    public function GetPublic($private)
+    {
+        try
+        {
+            $stm = $this->db->prepare("SELECT * FROM $this->table WHERE private = ?");
+            $stm->execute(array($private));
+
+            $this->response->setResponse(true);
+            $this->response->result = $stm->fetchAll();
+
+            return $this->response;
+        }
+        catch(Exception $e)
+        {
+            $this->response->setResponse(false, $e->getMessage());
+            return $this->response;
+        }
+    }
+
+    public function getAllWithTag($tag)
+    {
+        try
+        {
+            $result = array();
+
+            $stm = $this->db->prepare("SELECT * FROM $this->table WHERE content LIKE ?");
+
+            $stm->execute(array($tag));
+
+            $this->response->setResponse(true);
+            $this->response->result = $stm->fetch();
+
+            return $this->response;
+        }
+        catch(Exception $e)
+        {
+            $this->response->setResponse(false, $e->getMessage());
+            return $this->response;
+        }
     }
 }
--- app/route/user_route.php
+++ app/route/user_route.php
@@ -1,11 +1,12 @@
 <?php
+
 use App\Model\UserModel;
 
 $app->group('/user/', function () {
-    
+
     $this->get('test', function ($req, $res, $args) {
         return $res->getBody()
-                   ->write('Hello Users');
+            ->write('Hello Users');
     });
 
 
@@ -18,60 +19,90 @@ $app->group('/user/', function () {
                 '{"code": "200", "msg": "LSNote API v0.1"}'
             );
     });
-    
+
     $this->get('getAll', function ($req, $res, $args) {
         $um = new UserModel();
-        return $res->getBody()
-            ->write('Hello Users');
-        return $res
-           ->withHeader('Content-type', 'application/json')
-           ->getBody()
-           ->write(
-            json_encode(
-                $um->GetAll()
-            )
-        );
+
+        $result = $um->GetAll();
+        if (count($result->result) == 0) {
+            $arr = array('code' => 204, 'msg' => 'No notes found!');
+            return $res
+                ->withJson($arr, 204);
+        } else {
+            $arr = array('code' => 200, 'resp' => $result);
+            return $res
+                ->withJson($arr, 200);
+        }
     });
-    
-    $this->get('get/{id}', function ($req, $res, $args) {
+
+    $this->get('getOne/{id}', function ($req, $res, $args) {
         $um = new UserModel();
-        
-        return $res
-           ->withHeader('Content-type', 'application/json')
-           ->getBody()
-           ->write(
-            json_encode(
-                $um->Get($args['id'])
-            )
-        );
+        $result = $um->Get($args['id']);
+
+        if (count($result->result) == 0) {
+            $arr = array('code' => 204, 'msg' => 'This note does not exist!');
+            return $res
+                ->withJson($arr, 204);
+        } else {
+            $arr = array('code' => 200, 'resp' => $result);
+            return $res
+                ->withJson($arr, 200);
+        }
     });
-    
-    $this->post('save', function ($req, $res) {
+
+    $this->post('insert', function ($req, $res) {
         $um = new UserModel();
-        
-        return $res
-           ->withHeader('Content-type', 'application/json')
-           ->getBody()
-           ->write(
-            json_encode(
-                $um->InsertOrUpdate(
-                    $req->getParsedBody()
-                )
-            )
+
+        $um->InsertOrUpdate(
+            $req->getParsedBody()
         );
+
+        $arr = array('code' => 200, 'msg' => 'Note inserted!');
+        return $res
+            ->withJson($arr, 200);
+
     });
-    
+
     $this->post('delete/{id}', function ($req, $res, $args) {
         $um = new UserModel();
-        
+
+        $um->Delete($args['id']);
+
+        $arr = array('code' => 200, 'msg' => 'Note deleted!');
         return $res
-           ->withHeader('Content-type', 'application/json')
-           ->getBody()
-           ->write(
-            json_encode(
-                $um->Delete($args['id'])
-            )
-        );
+            ->withJson($arr, 200);
     });
-    
+
+    $this->get('getPublic', function ($req, $res, $args) {
+        $um = new UserModel();
+
+        $result = $um->GetPublic(false);
+        if (count($result->result) == 0) {
+            $arr = array('code' => 204, 'msg' => 'No notes found');
+            return $res
+                ->withJson($arr, 204);
+        } else {
+            $arr = array('code' => 200, 'resp' => $result);
+            return $res
+                ->withJson($arr, 200);
+        }
+
+    });
+
+    $this->get('getAllWithTag/{tag}', function ($req, $res, $args) {
+        $um = new UserModel();
+
+        $result = $um->getAllWithTag('%' . $args['tag'] . '%');
+
+        if (count($result->result) == 0) {
+            $arr = array('code' => 204, 'msg' => 'No notes found!');
+            return $res
+                ->withJson($arr, 204);
+        } else {
+            $arr = array('code' => 200, 'resp' => $result);
+            return $res
+                ->withJson($arr, 200);
+        }
+    });
+
 });
